8fa82872bf1c6d84dbfedad48f510e305fa3c3f9
Qyoh <111210018+Qyoh7@users.noreply.github.com>
2024-12-04 14:01:30 -0800

    set up theme pathing

--- content.js
+++ content.js
@@ -1,3 +1,24 @@
-ace.config.setModuleUrl("ace/theme/gruvbox", "https://qyoh7.github.io/ace-editor/src/theme/gruvbox.js");
+//theme pathing setup
+const baseThemeURL = "https://qyoh7.github.io/ace-editor/src/theme/"; 
+const themeFileNames = [
+    "gruvbox.js",
+    "github_dark.js",
+    "nord_dark.js",
+    "terminal.js",
+    "xcode.js",
+    "gruvbox.js",
+    "gruvbox.js",
+];
+const themeURLS = []
+
+for (let index = 0; index < themeFileNames.length; index++) 
+{
+    themeURLS.push(baseThemeURL.concat(themeFileNames[index]));    
+}
+
+for (let index = 0; index < themeURLS.length; index++) {
+    ace.config.setModuleUrl(themeFileNames.replace(".js", ""), themeURLS[index])
+}
 
 editor.setTheme("ace/theme/gruvbox");
+setKeyboardHandler(vim);
